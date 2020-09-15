efeef70292865302a7de7c7e732eb79dd39dc833
LeonardoViotti <leonardoviotti@gmail.com>
2020-09-15 17:49:10 -0400

    Changed config to run full data.

--- cdr-aggregation/config_telecel.py
+++ cdr-aggregation/config_telecel.py
@@ -18,14 +18,14 @@ datasource_configs = {
   # Schema
   "schema" : schema,
   # Folders containing data in base_path/
-  "data_paths" : ["wb_cdr_final_201901.txt"],
-  "filestub":"teleceltest",
+  "data_paths" : ["*.txt"],
+  "filestub":"full",
   # Select what type of environment, 'local', 'cluster' or 'hive'
   "spark_mode":"cluster",
   # Select files to be loaded. If you need to create the mappings and distances files, select just the first 3 files, these are not created by the code.
-  "geofiles": { "tower_sites":"sites-lookup.csv",
-                # "admin2":"ug_admin2_shapefile.csv",
-                # "admin3":"ug_admin3_shapefile.csv",
+  "geofiles": { "tower_sites":"sites-lookup-edited.csv",
+                "admin2":"zw_admin2_shapefile.csv",
+                "admin3":"zw_admin3_shapefile.csv",
                 # "voronoi":"ug_voronoi_shapefile.csv",
                 # "admin2_tower_map":"ug_admin2_tower_map.csv",
                 # "admin3_tower_map":"ug_admin3_tower_map.csv",
@@ -35,5 +35,5 @@ datasource_configs = {
   # Select levels to  
   "shapefiles": ['admin2','admin3'],
 # "shapefiles": ['admin2','admin3', 'voronoi'],
-  "dates": {'start_date' : dt.datetime(2020,1,1),
-            'end_date' : dt.datetime(2020,2,1)}}
+  "dates": {'start_date' : dt.datetime(2016,12,1),
+            'end_date' : dt.datetime(2019,2,28)}}
